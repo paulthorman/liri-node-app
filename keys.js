0aa5c37215b8dd8ad4e657a71e83bdd9e0139495
Paul Thorman <paulthorman@Pauls-MacBook-Pro.local>
2017-11-01 11:47:51 -0700

    files required

---
+++ keys.js
@@ -0,0 +1,11 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '9xiygjClQuRjctnC6hcv1Brhz',
+  consumer_secret: '<hDVAakL95fjjG8xvXYcMS3boFpJ7CVQaO0j0MlzU7q00jSxQ6P>',
+  access_token_key: '925757702780870657-xsWSAkXBB6ZAifZKamgiLwDzGhfYKyV',
+  access_token_secret: 'n4laAz8yx3HkKkRMieu99DVbifpm9Uqw5qbIvduJH9Y1a',
+}
+
+module.exports = twitterKeys;
+
